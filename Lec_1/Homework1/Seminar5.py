65967c34e6d45a1effc2c7d1816887e2eec31fdc
Dmitry Saenko <dm.saenko@gmail.com>
2023-01-20 13:10:49 +0300

    Sem_5_Tasks_Done

---
+++ Lec_1/Homework1/Seminar5.py
@@ -0,0 +1,13 @@
+# В файле находится N натуральных чисел, записанных через пробел. Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найдите это число.
+
+
+# string = '1 3 4 5 6 8 9 11 13'
+# my_list = string.split()
+# for i in range(len(my_list)):
+#     my_list[i]=int(my_list[i])
+# for i in range(1,len(my_list)):
+#     if my_list[i]-1 != my_list[i-1]:
+#         print(my_list[i]-1)
+
+
+
